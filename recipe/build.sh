33e1f51a66b2eda971a30c38db86db2c19f2df34
sdc50 <sdc50@byu.net>
2023-05-12 14:05:25 -0600

    debug

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,5 @@
-echo **********************  BUILDING!!! *********************
+echo "**********************  BUILDING!!! *********************"
 ls -l $SRC_DIR
 echo -e "[options]\npackages = find_namespace:\ninclude_package_data = True\n\n[options.packages.find]\ninclude = tethys_*" > $SRC_DIR/setup.cfg
-PBR_VERSION=$PKG_VERSION $PYTHON -m pip install . -vv
+echo "recursive-include tethys_* *" > $SRC_DIR/MANIFEST.in
+SETUPTOOLS_SCM_PRETEND_VERSION=$PKG_VERSION $PYTHON -m pip install . -vv
