92f0e0d75de8d90f19e018bb4698631a1f9a43ad
sdc50 <sdc50@byu.net>
2023-05-12 13:15:41 -0600

    debug

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,2 +1,4 @@
+echo $RECIPE_DIR
+ls -l $RECIPE_DIR
 echo -e "[options]\npackages = find_namespace:\ninclude_package_data = True\n\n[options.packages.find]\ninclude = tethys_*" > $RECIPE_DIR/setup.cfg
 PBR_VERSION=$PKG_VERSION $PYTHON -m pip install . -vv
