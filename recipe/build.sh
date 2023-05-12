0672883affa3896d9f0ae35c4269c8807b83673d
sdc50 <sdc50@byu.net>
2023-05-12 12:50:43 -0600

    test new setup.cfg

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,1 +1,2 @@
+echo -e "[options]\npackages = find_namespace:\ninclude_package_data = True\n\n[options.packages.find]\ninclude = tethys_*" > $RECIPE_DIR/setup.cfg
 PBR_VERSION=$PKG_VERSION $PYTHON -m pip install . -vv
