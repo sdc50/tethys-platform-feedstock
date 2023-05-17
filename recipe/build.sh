95db56c286a0c17943e356e5c56530e7a087ffc9
sdc50 <sdc50@byu.net>
2023-05-17 12:24:36 -0600

    v4.1.0 (#10)
    * updated v4.1.0
    
    * Update dependencies
    
    * move build script to yaml
    
    * Delete build.sh
    
    * Delete bld.bat
    
    ---------
    
    Co-authored-by: regro-cf-autotick-bot <36490558+regro-cf-autotick-bot@users.noreply.github.com>

--- recipe/build.sh
+++
@@ -1,5 +0,0 @@
-echo "**********************  BUILDING!!! *********************"
-ls -l $SRC_DIR
-echo -e "[options]\npackages = find_namespace:\ninclude_package_data = True\n\n[options.packages.find]\ninclude = tethys_*" > $SRC_DIR/setup.cfg
-echo "recursive-include tethys_* *" > $SRC_DIR/MANIFEST.in
-SETUPTOOLS_SCM_PRETEND_VERSION=$PKG_VERSION $PYTHON -m pip install . -vv
